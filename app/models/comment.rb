22a92733e4a967d11417ddeceff6ded3c1282b88
Leah Seyoum <leahseyoum@gmail.com>
2023-06-13 19:21:58 -0700

    comment css fixes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
     validates :description, :commentor_id, :pin_id, presence: true
+    validates :description, length: { maximum: 150 }
 
     belongs_to :commentor, class_name: 'User'
     belongs_to :pin
--- frontend/src/components/Comments/CommentContainer.js
+++ frontend/src/components/Comments/CommentContainer.js
@@ -1,53 +1,57 @@
+
 import './CommentsContainer.css';
 import CommentInput from './CommentInput';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useSelector } from 'react-redux';
 import SingleComment from './SingleComment';
 
 const CommentContainer = ({ pin, user }) => {
-    const [comments, setComments] = useState([])
-    const pin2 = useSelector(state => state.pins.pin)
-    const commentsObj = pin2?.comments;
+  const [comments, setComments] = useState([]);
+  const commentsContainerRef = useRef(null);
+  const pin2 = useSelector(state => state.pins.pin);
+  const commentsObj = pin2?.comments;
 
-    useEffect(() => {
-        if (commentsObj) {
-            const commentsArr = Object.values(commentsObj);
-            const processedComments = commentsArr?.map(comment => ({
-                id: comment.id,
-                description: comment.description,
-                commenterId: comment.commentorId,
-                createdAt: comment.createdAt
-            } ));
-              setComments(processedComments);
-        } else if (commentsObj === undefined) {
-            setComments([]);
-        }
-    }, [commentsObj, pin2])
+  useEffect(() => {
+    if (commentsObj) {
+      const commentsArr = Object.values(commentsObj);
+      const processedComments = commentsArr?.map(comment => ({
+        id: comment.id,
+        description: comment.description,
+        commenterId: comment.commentorId,
+        createdAt: comment.createdAt
+      }));
+      setComments(processedComments);
+    } else if (commentsObj === undefined) {
+      setComments([]);
+    }
+  }, [commentsObj, pin2]);
 
-    return (
-        <>  
-            <div className='comment-component-container'>
-            <h1 className='num-comments'>
-            {comments.length === 1 ? `${comments.length} Comment` : `${comments.length} Comments`}
-            </h1>
+  useEffect(() => {
+    scrollToBottom();
+  }, [comments]);
 
-                
-                    <div className='comment-list'>
-                        {
-                            comments.map((comment, i) => <SingleComment
-                                key={i}
-                                pin={pin}
-                                comment={comment}
-                                user={user}
-                            />)
-                        }
-                    </div>
+  const scrollToBottom = () => {
+    commentsContainerRef.current.scrollIntoView({ behavior: 'smooth', block: 'end' });
+  };
 
-               
-                <CommentInput pin={pin} user={user}/>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className='comment-component-container'>
+        <h1 className='num-comments'>
+          {comments.length === 1 ? `${comments.length} Comment` : `${comments.length} Comments`}
+        </h1>
+
+        <div className='comment-list'>
+          {comments.map((comment, i) => (
+            <SingleComment key={i} pin={pin} comment={comment} user={user} />
+          ))}
+          <div ref={commentsContainerRef} />
+        </div>
+
+        <CommentInput pin={pin} user={user} />
+      </div>
+    </>
+  );
 };
 
-export default CommentContainer
+export default CommentContainer;
